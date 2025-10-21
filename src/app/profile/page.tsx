9bbc93854e0963358cff341cf594dc9a67dd6a8a
NelsonFrancis <developer.nelson22@gmail.com>
2025-10-21 16:22:07 +0400

    logout func done

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,21 @@
+"use client"
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage(){
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            router.push("/login");
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return(
-        <h1>Profile page</h1>
+        <>
+            <h1>Profile page</h1>
+            <button onClick={logout} className="logout">Logout</button>
+        </>
     )
 }
